b9db61fd58eb5d4103e4a00c238538fab3f9e084
Riccardo <riccardo.manzoni@cern.ch>
2014-06-10 22:09:09 +0200

    added option to merge ttH WH and ZH into VH for custom datacard

--- customDatacard.py
+++ customDatacard.py
@@ -7,7 +7,7 @@ from ROOT     import TFile, gDirectory
 
 parser = OptionParser()
 parser.usage = '''
-customDatacard.py folder1:name_of_the_category1 folder2:name_of_the_category2 ... [OPTIONS] 
+customDatacard.py folder1:name_of_the_category1 folder2:name_of_the_category2 ... [OPTIONS]
 
 EXAMPLE:
 customDatacard.py CMS_2012_tight_VBF_nom:tight_vbf CMS_2012_tight_BOOSTED_nom:tight_boost CMS_2012_loose_BOOSTED_nom:loose_boost CMS_2012_loose_VBF_nom:loose_vbf
@@ -16,6 +16,8 @@ parser.add_option("-N", "--shift" , dest = "shift" ,  help = "save the up, down 
 parser.add_option("-S", "--susy"  , dest = "susy"  ,  help = "susy datacard. Default is False"              , default = False      )
 parser.add_option("-F", "--file"  , dest = "file"  ,  help = "filename. Default is '' "                     , default = ''         )
 parser.add_option("-M", "--mass"  , dest = "mass"  ,  help = "mass. Default is svfitMass, optional visMass" , default = 'svfitMass')
+parser.add_option("-V", "--vh"    , dest = "vh"    ,  help = "merge ttH, WH and ZH into VH"                 , default = False      )
+
 (options,av) = parser.parse_args()
 
 print av
@@ -30,22 +32,22 @@ if options.mass == 'visMass' :
 if options.file != '' : filename = options.file
 
 hists = {}
- 
+
 for category in av :
-  
+
   cat_fold = category.split(':')[0]
   cat_name = category.split(':')[1]
-  
+
   if options.shift :
     for n in ['_nom','_up','_down'] :
       cat_fold = cat_fold.replace(n,'')
-  
+
   else :
     for n in ['_nom'] :
       cat_fold = cat_fold.replace(n,'')
 
   hists.update({cat_name:[]})
-  
+
   if 'BOOSTED' in cat_fold : cat = 'boost'
   if 'VBF'     in cat_fold : cat = 'vbf'
 
@@ -59,16 +61,26 @@ for category in av :
     if 'ZTT' in hh.GetName() or 'QCD' in hh.GetName() :
       for bin in range(hh.GetNbinsX()+1) :
         if hh.GetBinError(bin) == 0 :
-          hh.SetBinError(bin, 1.)  
+          hh.SetBinError(bin, 1.)
+    if h1.GetName() in ['ttH','WH','ZH'] and options.vh :
+      sum_vh.append(dc(h2))
+      continue
+    if options.vh :
+      vh = dc(sum_vh[0])
+      vh.Add(sum_vh[1])
+      vh.Add(sum_vh[2])
+      vh.SetName('VH')
+      hists[cat_name].append(vh)
     hists[cat_name].append(hh)
   f1.Close()
-  
-  
+
+
   if options.shift :
     f2 = TFile(cat_fold+'_up/'+cat_fold+'_up_tauTau_'+cat+'_'+options.mass+'.root','read')
     dir = 'tauTau_'+cat
     f2.cd(dir)
     dirList = gDirectory.GetListOfKeys()
+    sum_vh = []
     for k2 in dirList:
       h2 = k2.ReadObj()
       if 'ZTT' in h2.GetName() or \
@@ -79,16 +91,26 @@ for category in av :
          'ZH'  in h2.GetName() or \
          'ttH' in h2.GetName() or \
          'bbH' in h2.GetName() or \
-         'QCD' in h2.GetName() : 
+         'QCD' in h2.GetName() :
+        if h2.GetName() in ['ttH','WH','ZH'] and options.vh :
+          sum_vh.append(dc(h2))
+          continue
         hh = dc(h2)
         hh.SetName(hh.GetName()+'_CMS_scale_t_tautau_8TeVUp')
         hists[cat_name].append(hh)
+      if options.vh :
+        vh = dc(sum_vh[0])
+        vh.Add(sum_vh[1])
+        vh.Add(sum_vh[2])
+        vh.SetName('VH_CMS_scale_t_tautau_8TeVUp')
+        hists[cat_name].append(vh)
     f2.Close()
 
     f3 = TFile(cat_fold+'_down/'+cat_fold+'_down_tauTau_'+cat+'_'+options.mass+'.root','read')
     dir = 'tauTau_'+cat
     f3.cd(dir)
     dirList = gDirectory.GetListOfKeys()
+    sum_vh = []
     for k3 in dirList:
       h3 = k3.ReadObj()
       if 'ZTT' in h3.GetName() or \
@@ -100,9 +122,18 @@ for category in av :
          'ttH' in h3.GetName() or \
          'bbH' in h3.GetName() or \
          'QCD' in h3.GetName() :
+        if h3.GetName() in ['ttH','WH','ZH'] and options.vh :
+          sum_vh.append(dc(h2))
+          continue
         hh = dc(h3)
         hh.SetName(hh.GetName()+'_CMS_scale_t_tautau_8TeVDown')
         hists[cat_name].append(hh)
+      if options.vh :
+        vh = dc(sum_vh[0])
+        vh.Add(sum_vh[1])
+        vh.Add(sum_vh[2])
+        vh.SetName('VH_CMS_scale_t_tautau_8TeVDown')
+        hists[cat_name].append(vh)
     f3.Close()
 
 #print [ h.GetName() for h in hists['boost_tight']]
@@ -111,9 +142,9 @@ tot = TFile(filename,'recreate')
 for d in hists.keys():
   tot.mkdir('tauTau_'+d)
   tot.cd('tauTau_'+d)
-  
+
   alreadyIn = []
-  
+
   for h in hists[d] :
     if h.GetName() not in alreadyIn:
       if '_fine_binning' in h.GetName() :
