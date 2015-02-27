fb6f9db8edce9c5820def1a3e722de330c940e27
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2015-02-27 15:09:12 +0100

    fixed typo, missing 100 factor in percentages

--- StatUncScript.py
+++ StatUncScript.py
@@ -11,10 +11,10 @@ for i in collection :
   integral = myGet.getContributionUnc(i[0],i[1],i[2])[0]
   error    = myGet.getContributionUnc(i[0],i[1],i[2])[1]
   
-#   if error/integral < 0.05 :
-#     continue
+  if error/integral < 0.05 :
+    continue
     
   print '\n', i[0],i[1],i[2]
   print 'integral %.2f +/- %.2f' % (integral, error) 
-  print 'relative error %.2f%s' % ( error/integral, '%' )
+  print 'relative error %.2f%s' % ( 100*error/integral, '%' )
 
